e6f857c3bd0ce5c1b246a3f6f7695dee6092af99
nmschu <nm_schuler@yahoo.com>
2016-02-01 18:47:29 -0500

    using directives to clean up and organize the text

--- homework4/app.js
+++ homework4/app.js
@@ -1,26 +1,43 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
-  });
+
+
+app.directive("productGallery", function(){
+  return{
+    restrict: 'E',
+    templateUrl: "product-gallery.html",
+    controller: function(){
+      this.current = 0;
+      this.setCurrent = function(imageNumber){
+        this.current = imageNumber || 0;
+      };
+    },
+    controllerAs: "gallery"
+  };
+});
+
 
   app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller("TabController", function() {
-    this.tab = 1;
+  app.directive("productTabs", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-tabs.html",
+      controller:function(){
+          this.tab = 1;
 
-    this.isSet = function(checkTab) {
-      return this.tab === checkTab;
-    };
+          this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+          };
 
-    this.setTab = function(setTab) {
-      this.tab = setTab;
+          this.setTab = function(setTab) {
+            this.tab = setTab;
+          };
+      },
+      controllerAs: "tab"
     };
   });
 
@@ -35,10 +52,10 @@
 
   });
 
-  app.directive("productDescriptions", function() {
+  app.directive("productDescription", function() {
     return {
       restrict: 'E',
-      templateUrl: "product-descriptions.html"
+      templateUrl: "product-description.html"
     };
   });
 
@@ -49,9 +66,9 @@
     };
   });
 
-  app.directive("productSpecs", function(){
+  app.directive("productSpecs", function() {
     return {
-      restrict: 'A',
+      restrict:"A",
       templateUrl: "product-specs.html"
     };
   });
